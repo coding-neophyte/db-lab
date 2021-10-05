9cbb740508c31d646ac30e1810458bc6a75c2a94
Julius Franklin <jjfranklin88@outlook.com>
2021-10-04 21:48:34 -0400

    new routes with join and tdd

--- data/ball-teams.js
+++ data/ball-teams.js
@@ -4,28 +4,28 @@ module.exports = [
     city: 'new york',
     logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/25/New_York_Knicks_logo.svg/1200px-New_York_Knicks_logo.svg.png',
     championships: 2,
-    category: 'eastern conference'
+    category_id: 1
   },
   {
     name: 'trailblazers',
     city: 'portland',
     logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/21/Portland_Trail_Blazers_logo.svg/1200px-Portland_Trail_Blazers_logo.svg.png',
     championships: 1,
-    category: 'western conference'
+    category_id: 2
   },
   {
     name: 'heat',
     city: 'miami',
     logo: 'https://blog.logomyway.com/wp-content/uploads/2021/07/miami-heat-logo.png',
     championships: 3,
-    category: 'eastern conference'
+    category_id: 1
   },
   {
     name: 'warriors',
     city: 'golden state',
     logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/01/Golden_State_Warriors_logo.svg/1200px-Golden_State_Warriors_logo.svg.png',
     championships: 6,
-    category: 'western conference'
+    category_id: 2
 
   }
 ];
